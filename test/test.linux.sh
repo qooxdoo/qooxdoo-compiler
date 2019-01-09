fca2fc6a5b1f0d1ce8d0d39048e3cea87e9d1c2a
Henner Kollmann <henner.kollmann@gmx.de>
2019-01-09 10:20:00 +0100

    fix test script

--- test/test.linux.sh
+++ test/test.linux.sh
@@ -4,7 +4,7 @@ qx create myapp -I --type server -v || exit $?
 cd myapp
 qx compile -v --clean || exit $?
 node source-output/myapp/myapp.js || exit $?
-qx config set github.token $GH_TOKEN || exit $?
+qx config set github.token ${NPM_TOKEN} || exit $?
 qx contrib update --search -v|| exit $?
 qx contrib list    -v|| exit $?
 qx contrib install oetiker/UploadWidget -v || exit $?
