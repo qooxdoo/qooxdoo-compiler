307c537538c64d9b3ba64339946f793a9f54bbe9
Christian Boulanger <info@bibliograph.org>
2019-04-08 08:22:41 +0200

    Add documentation [ci skip]

--- test/test.linux.sh
+++ test/test.linux.sh
@@ -1,13 +1,18 @@
 set -x
+echo "Testing qooxdoo-compiler version $(qx --version)"
+echo
 rm -rf myapp
 # test create app
 qx create myapp -I --type server -v || exit $?
 cd myapp
 qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
+# test qx contrib list
+qx contrib update  -v || exit $?
+qx contrib list    -v || exit $?
+qx contrib list --all --short --noheaders || exit $?
+qx contrib list --json --match==qooxdoo/ || exit $?
 # test add contrib
-qx contrib update  -v|| exit $?
-qx contrib list    -v|| exit $?
 qx contrib install oetiker/UploadWidget -v --release v1.0.1 || exit $?
 qx contrib install cboulanger/qx-contrib-Dialog -v || exit $?
 qx contrib install johnspackman/UploadMgr -v || exit $?
@@ -15,21 +20,25 @@ qx contrib install ergobyte/qookery/qookeryace -v || exit $?
 qx contrib install ergobyte/qookery/qookerymaps -v || exit $?
 qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
+qx contrib list --installed --short --noheaders
 # test reinstall contrib
 qx clean || exit $?
 qx contrib install -v || exit $?
 qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js
+qx contrib list -isH
 # test remove contrib
 qx contrib remove oetiker/UploadWidget -v || exit $?
 qx contrib remove ergobyte/qookery/qookeryace -v || exit $?
 qx contrib remove ergobyte/qookery/qookerymaps -v || exit $?
 qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
+qx contrib list --installed --short --noheaders
 # test install without manifest
 qx contrib install ergobyte/qookery -v || exit $?
 qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
+qx contrib list --installed --short --noheaders
 # test add class and add script
 qx add class myapp.Window --extend=qx.ui.window.Window || exit $?
 qx add script ../testdata/npm/script/jszip.js --rename=zip.js || exit $?
