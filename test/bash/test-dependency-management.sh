a44032757d83552537066359507205d8d8ae810b
Tobias Oetiker <tobi@oetiker.ch>
2019-04-14 17:42:56 +0200

    Make tests work locally

--- test/bash/test-dependency-management.sh
+++ test/bash/test-dependency-management.sh
@@ -3,57 +3,58 @@
 echo
 echo "Test 1: qxl.test1, latest version"
 [[ -d myapp ]] && rm -rf myapp
-qx create myapp -I
-cd myapp
-qx contrib install qooxdoo/qxl.test1 --verbose
-LIST=$(qx contrib list --short --noheaders --installed --all)
+./qx create myapp -I
+pushd myapp
+../qx contrib install qooxdoo/qxl.test1 --verbose
+LIST=$(../qx contrib list --short --noheaders --installed --all)
 echo "$LIST"
 COUNTLINES=$(echo "$LIST" | wc -l | tr -d ' ')
 if [ "$COUNTLINES" != "3" ]; then echo "Installing dependencies failed"; exit 1; fi
-qx compile --feedback=false --warnAsError || exit 1
-cd ..
+../qx compile --feedback=false --warnAsError || exit 1
+popd
 
 echo
 echo "Test 2: qxl.test2/qxl.test2A, latest version"
 rm -rf myapp
-qx create myapp -I
-cd myapp
-qx contrib install qooxdoo/qxl.test2/qxl.test2A --verbose
-LIST=$(qx contrib list --short --noheaders --installed --all)
+./qx create myapp -I
+pushd myapp
+../qx contrib install qooxdoo/qxl.test2/qxl.test2A --verbose
+LIST=$(../qx contrib list --short --noheaders --installed --all)
 echo "$LIST"
 COUNTLINES=$(echo "$LIST" | wc -l | tr -d ' ')
 if [ "$COUNTLINES" != "4" ]; then echo "Installing dependencies failed"; exit 1; fi
-qx compile --feedback=false --warnAsError || exit 1
-cd ..
+../qx compile --feedback=false --warnAsError || exit 1
+popd
 
 echo
 echo "Test 3: qxl.test1@v1.0.2 version"
 rm -rf myapp
-qx create myapp -I
-cd myapp
-qx contrib install qooxdoo/qxl.test1@v1.0.2 --verbose
-LIST=$(qx contrib list --short --noheaders --installed --all)
+./qx create myapp -I
+pushd myapp
+../qx contrib install qooxdoo/qxl.test1@v1.0.2 --verbose
+LIST=$(../qx contrib list --short --noheaders --installed --all)
 echo "$LIST"
 EXPECTED="\
 qooxdoo/qxl.test1              v1.0.2   v1.0.2   v1.0.2
 qooxdoo/qxl.test2/qxl.test2C   v1.0.2   v1.0.2   v1.0.2
 qooxdoo/qxl.test2/qxl.test2D   v1.0.2   v1.0.2   v1.0.2"
 if [ "$EXPECTED" != "$LIST" ]; then echo "Installing dependencies failed"; exit 1; fi
-qx compile --feedback=false --warnAsError || exit 1
-cd ..
+../qx compile --feedback=false --warnAsError || exit 1
+popd
+
 
 echo
 echo "Test 4: qxl.test1@e27ecf811667c1b3bc5dc023d806e74a9328a26c"
 rm -rf myapp
-qx create myapp -I
-cd myapp
-qx contrib install qooxdoo/qxl.test1@e27ecf811667c1b3bc5dc023d806e74a9328a26c --verbose
-LIST=$(qx contrib list --short --noheaders --installed --all)
+./qx create myapp -I
+pushd myapp
+../qx contrib install qooxdoo/qxl.test1@e27ecf811667c1b3bc5dc023d806e74a9328a26c --verbose
+LIST=$(../qx contrib list --short --noheaders --installed --all)
 echo "$LIST"
 EXPECTED="\
 qooxdoo/qxl.test1              e27ecf811667c1b3bc5dc023d806e74a9328a26c   v1.0.2   v1.0.2
 qooxdoo/qxl.test2/qxl.test2C   v1.0.2                                     v1.0.2   v1.0.2
 qooxdoo/qxl.test2/qxl.test2D   v1.0.2                                     v1.0.2   v1.0.2"
 if [ "$EXPECTED" != "$LIST" ]; then echo "Installing dependencies failed"; exit 1; fi
-qx compile --feedback=false --warnAsError || exit 1
-cd ..
+../qx compile --feedback=false --warnAsError || exit 1
+popd
--- test/test.linux.sh
+++ test/test.linux.sh
@@ -1,59 +1,60 @@
 #!/usr/bin/env bash
 set -x
-echo "Testing qooxdoo-compiler version $(qx --version)"
+
+echo "Testing qooxdoo-compiler version $(./qx --version)"
 echo
 
-cd test
+pushd test
 node test-deps.js
-cd ..
+popd
 
-qx contrib update || exit $?
+./qx contrib update || exit $?
 bash test/bash/test-dependency-management.sh || exit $?
 
 rm -rf myapp
 # test create app
-qx create myapp -I --type server -v || exit $?
-cd myapp
-qx compile -v --clean || exit $?
+./qx create myapp -I --type server -v || exit $?
+pushd myapp
+../qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
-# test qx contrib list
-qx contrib update  -v || exit $?
-qx contrib list    -v || exit $?
-qx contrib list --all --short --noheaders --match=qooxdoo/ || exit $?
-qx contrib list --json --installed || exit $?
+# test ./qx contrib list
+../qx contrib update  -v || exit $?
+../qx contrib list    -v || exit $?
+../qx contrib list --all --short --noheaders --match=qooxdoo/ || exit $?
+../qx contrib list --json --installed || exit $?
 # test add contrib
-qx contrib install oetiker/UploadWidget -v --release v1.0.1 || exit $?
-qx contrib install cboulanger/qx-contrib-Dialog -v || exit $?
-qx contrib install johnspackman/UploadMgr -v || exit $?
-qx contrib install ergobyte/qookery/qookeryace -v || exit $?
-qx contrib install ergobyte/qookery/qookerymaps -v || exit $?
-qx compile -v --clean || exit $?
+../qx contrib install oetiker/UploadWidget -v --release v1.0.1 || exit $?
+../qx contrib install cboulanger/./qx-contrib-Dialog -v || exit $?
+../qx contrib install johnspackman/UploadMgr -v || exit $?
+../qx contrib install ergobyte/qookery/qookeryace -v || exit $?
+../qx contrib install ergobyte/qookery/qookerymaps -v || exit $?
+../qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
-qx contrib list --installed --short --noheaders
+../qx contrib list --installed --short --noheaders
 # test reinstall contrib
-qx clean || exit $?
-qx contrib install -v || exit $?
-qx compile -v --clean || exit $?
+../qx clean || exit $?
+../qx contrib install -v || exit $?
+../qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js
-qx contrib list -isH
+../qx contrib list -isH
 # test remove contrib
-qx contrib remove oetiker/UploadWidget -v || exit $?
-qx contrib remove ergobyte/qookery/qookeryace -v || exit $?
-qx contrib remove ergobyte/qookery/qookerymaps -v || exit $?
-qx compile -v --clean || exit $?
+../qx contrib remove oetiker/UploadWidget -v || exit $?
+../qx contrib remove ergobyte/qookery/qookeryace -v || exit $?
+../qx contrib remove ergobyte/qookery/qookerymaps -v || exit $?
+../qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
-qx contrib list --installed --short --noheaders
+../qx contrib list --installed --short --noheaders
 # test install without manifest
-qx clean || exit $?
-qx contrib install ergobyte/qookery -v || exit $?
-qx compile -v --clean || exit $?
+../qx clean || exit $?
+../qx contrib install ergobyte/qookery -v || exit $?
+../qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
-qx contrib list --installed --short --noheaders
+../qx contrib list --installed --short --noheaders
 # test add class and add script
-qx add class myapp.Window --extend=qx.ui.window.Window || exit $?
-qx add script ../testdata/npm/script/jszip.js --rename=zip.js || exit $?
+../qx add class myapp.Window --extend=./qx.ui.window.Window || exit $?
+../qx add script ../testdata/npm/script/jszip.js --rename=zip.js || exit $?
 cp ../testdata/npm/application/*.js source/class/myapp
-qx lint --fix --warnAsError ||  exit $?
-qx compile -v --clean || exit $?
+../qx lint --fix --warnAsError ||  exit $?
+../qx compile -v --clean || exit $?
 node compiled/source/myapp/myapp.js || exit $?
-
+popd
