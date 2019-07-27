1bb16bf28bff8df06641ed6d3a6796029c6a550e
Tobias Oetiker <tobi@oetiker.ch>
2019-07-27 10:38:49 +0200

    fix object case

--- source/class/qx/tool/cli/commands/package/Migrate.js
+++ source/class/qx/tool/cli/commands/package/Migrate.js
@@ -165,7 +165,10 @@ qx.Class.define("qx.tool.cli.commands.package.Migrate", {
                 } else if (qx.lang.Type.isString(authors)) {
                   return [{name: authors}];
                 } else if (qx.lang.Type.isObject(authors)) {
-                  return [authors];
+                  return [{
+                    name: authors.name,
+                    email: authors.email
+                  }];
                 } else if (qx.lang.Type.isArray(authors)) {
                   return authors.map( r =>  
                     qx.lang.Type.isObject(r) ? {
