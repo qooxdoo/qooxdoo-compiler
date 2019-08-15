944a295f5cd58f7b32efb2d4df594ae629b99799
Henner Kollmann <henner.kollmann@gmx.de>
2019-08-15 22:17:26 +0200

    message "qx.tool.cli.compile.unusedTarget" isundefined

--- source/class/qx/tool/cli/commands/Compile.js
+++ source/class/qx/tool/cli/commands/Compile.js
@@ -588,7 +588,7 @@ qx.Class.define("qx.tool.cli.commands.Compile", {
       let makers = [];
       targetConfigs.forEach(targetConfig => {
         if (!targetConfig.appConfigs) {
-          qx.tool.compiler.Console.print("qx.tool.cli.compile.unusedTarget", target.type, target.index);
+          qx.tool.compiler.Console.print("qx.tool.cli.compile.unusedTarget", targetConfig.type, targetConfig.index);
           return;
         }
 
@@ -993,6 +993,7 @@ qx.Class.define("qx.tool.cli.commands.Compile", {
       "qx.tool.cli.compile.makeEnds": "Applications are made"
     });
     qx.tool.compiler.Console.addMessageIds({
+      "qx.tool.cli.compile.unusedTarget": "Target type %1, index %2 is unused",
       "qx.tool.cli.compile.legacyFiles": "File %1 exists but is no longer used",
       "qx.tool.cli.compile.deprecatedCompile": "The configuration setting %1 in compile.json is deprecated",
       "qx.tool.cli.compile.deprecatedCompileSeeOther": "The configuration setting %1 in compile.json is deprecated (see %2)",
