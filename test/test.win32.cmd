24d90fbd1dca8d7d05d314a9d2cc720bf3cf5716
Henner Kollmann <henner.kollmann@gmx.de>
2019-09-02 21:59:50 +0200

    bring some color to output

--- test/test.win32.cmd
+++ test/test.win32.cmd
@@ -1,21 +1,22 @@
+@echo [101;93m check version [0m
 call npx qx --version
 @echo on
-@echo node API tests
-cd test
+@echo [101;93m node API tests [0m
+@cd test
 call node test-deps.js           || EXIT /B 1
 call node test-config-schemas.js || EXIT /B 1
 call node test-pkg-migrate.js    || EXIT /B 1
 call node test-commands.js       || EXIT /B 1
 call node test-cli.js            || EXIT /B 1
-@echo setup
+@echo [101;93m setup [0m
 cd ..
 rmdir  /Q /S myapp
-@echo "test create app"
+@echo [101;93m test create app [0m
 call npx qx create myapp --type server -I  -v || EXIT /B 1
 cd myapp
 call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
-@echo "test add package"
+@echo [101;93m test add package [0m
 call npx qx package update -v || EXIT /B 1
 call npx qx package list -v || EXIT /B 1
 call npx qx package install oetiker/UploadWidget -v --release v1.0.1 || EXIT /B 1
@@ -25,29 +26,29 @@ call npx qx package install ergobyte/qookery/qookeryace -v || EXIT /B 1
 call npx qx package install ergobyte/qookery/qookerymaps -v || EXIT /B 1
 call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
-@echo "test reinstall package"
+@echo [101;93m test reinstall package [0m
 call npx qx clean
 call npx qx package install  -v || EXIT /B 1
 call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
-@echo test remove package
+@echo [101;93m test remove package [0m
 call npx qx package remove oetiker/UploadWidget -v || EXIT /B 1
 call npx qx package remove ergobyte/qookery/qookeryace -v || EXIT /B 1
 call npx qx package remove ergobyte/qookery/qookerymaps -v || EXIT /B 1
 call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
-@echo test install without manifest
+@echo [101;93m test install without manifest [0m
 call npx qx clean
 call npx qx package install ergobyte/qookery -v || EXIT /B 1
 call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
-@echo test add class and add script
+@echo [101;93m test add class and add script [0m
 call npx qx add class myapp.Window --extend=qx.ui.window.Window --force || EXIT /B 1
 call npx qx add script ..\test\testdata\npm\script\jszip.js --rename=zip.js || EXIT /B 1
 copy ..\test\testdata\npm\application\*.js source\class\myapp /Y
 call npx qx lint --fix --warnAsError || EXIT /B 1
 call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
-@echo cleanup
+@echo [101;93m cleanup [0m
 cd ..
 rmdir  /Q /S myapp
