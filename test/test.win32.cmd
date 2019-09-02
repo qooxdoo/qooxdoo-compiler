9b46de942f7c0d87632c76bc91723978d6894308
Henner Kollmann <henner.kollmann@gmx.de>
2019-09-02 11:59:38 +0200

    use npx instead of qx global

--- test/test.win32.cmd
+++ test/test.win32.cmd
@@ -1,5 +1,5 @@
-call npm link
 
+call npx qx --version
 
 :: node API tests
 cd test
@@ -11,42 +11,42 @@ cd ..
 
 rmdir  /Q /S myapp
 :: test create app
-call qx create myapp --type server -I  -v || EXIT /B 1
+call npx qx create myapp --type server -I  -v || EXIT /B 1
 cd myapp
-call ..\qx compile -v --clean || EXIT /B 1
+call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
 :: test add package
-call ..\qx package update -v || EXIT /B 1
-call ..\qx package list -v || EXIT /B 1
-call ..\qx package install oetiker/UploadWidget -v || EXIT /B 1
-call ..\qx package install cboulanger/qx-contrib-Dialog -v || EXIT /B 1
-call ..\qx package install johnspackman/UploadMgr -v || EXIT /B 1
-call ..\qx package install ergobyte/qookery/qookeryace -v || EXIT /B 1
-call ..\qx package install ergobyte/qookery/qookerymaps -v || EXIT /B 1
-call ..\qx compile -v --clean || EXIT /B 1
+call npx qx package update -v || EXIT /B 1
+call npx qx package list -v || EXIT /B 1
+call npx qx package install oetiker/UploadWidget -v || EXIT /B 1
+call npx qx package install cboulanger/qx-contrib-Dialog -v || EXIT /B 1
+call npx qx package install johnspackman/UploadMgr -v || EXIT /B 1
+call npx qx package install ergobyte/qookery/qookeryace -v || EXIT /B 1
+call npx qx package install ergobyte/qookery/qookerymaps -v || EXIT /B 1
+call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
 :: test reinstall package
-call ..\qx clean
-call ..\qx package install  -v || EXIT /B 1
-call ..\qx compile -v --clean || EXIT /B 1
+call npx qx clean
+call npx qx package install  -v || EXIT /B 1
+call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
 :: test remove package
-call ..\qx package remove oetiker/UploadWidget -v || EXIT /B 1
-call ..\qx package remove ergobyte/qookery/qookeryace -v || EXIT /B 1
-call ..\qx package remove ergobyte/qookery/qookerymaps -v || EXIT /B 1
-call ..\qx compile -v --clean || EXIT /B 1
+call npx qx package remove oetiker/UploadWidget -v || EXIT /B 1
+call npx qx package remove ergobyte/qookery/qookeryace -v || EXIT /B 1
+call npx qx package remove ergobyte/qookery/qookerymaps -v || EXIT /B 1
+call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
 :: test install without manifest
-call ..\qx clean
-call ..\qx package install ergobyte/qookery -v || EXIT /B 1
-call ..\qx compile -v --clean || EXIT /B 1
+call npx qx clean
+call npx qx package install ergobyte/qookery -v || EXIT /B 1
+call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
 :: test add class and add script
-call ..\qx add class myapp.Window --extend=qx.ui.window.Window --force || EXIT /B 1
-call ..\qx add script ..\test\testdata\npm\script\jszip.js --rename=zip.js || EXIT /B 1
+call npx qx add class myapp.Window --extend=qx.ui.window.Window --force || EXIT /B 1
+call npx qx add script ..\test\testdata\npm\script\jszip.js --rename=zip.js || EXIT /B 1
 copy ..\test\testdata\npm\application\*.js source\class\myapp /Y
-call ..\qx lint --fix --warnAsError || EXIT /B 1
-call ..\qx compile -v --clean || EXIT /B 1
+call npx qx lint --fix --warnAsError || EXIT /B 1
+call npx qx compile -v --clean || EXIT /B 1
 call node compiled\source\myapp\myapp.js || EXIT /B 1
 :: delete the app
 cd ..
