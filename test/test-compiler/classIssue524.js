fbab34b9feabdc3fe716be66b21936d421ba235a
Henner Kollmann <henner.kollmann@gmx.de>
2019-09-16 22:07:05 +0200

    add tests for issue #524

---
+++ test/test-compiler/classIssue524.js
@@ -0,0 +1,21 @@
+qx.Class.define("classIssue524",
+{
+  extend : qx.application.Standalone,
+  members :
+  {
+    main : function() {}
+  }
+});
+
+qx.Class.define("some.Class",
+{
+  extend : qx.ui.container.Composite,
+  /**
+   * @ignore(init)
+   */
+   // test
+  construct : function()
+  {
+    init();
+  }
+});
