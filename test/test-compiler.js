a672028b488dc36b4ba2c53e3e7ed9df0a39f3e0
Henner Kollmann <henner.kollmann@gmx.de>
2019-09-17 12:47:42 +0200

    Fix for https://github.com/qooxdoo/qooxdoo-compiler/issues/524 (#571)
    * add tests for issue #524
    * leading comments are part of the property - so merge them

--- test/test-compiler.js
+++ test/test-compiler.js
@@ -1,14 +1,15 @@
 var test = require("tape");
 require("../index");
 
+var lib = new qx.tool.compiler.app.Library();
+var analyser = new qx.tool.compiler.Analyser();
+analyser.setOutputDir(".");
+lib.setRootDir(".");
+lib.setSourcePath("test-compiler");
+lib.getSourceFileExtension = () => ".js";
+
 test("test issue 519", assert => {
   try {  
-    var lib = new qx.tool.compiler.app.Library();
-    var analyser = new qx.tool.compiler.Analyser();
-    analyser.setOutputDir(".");
-    lib.setRootDir(".");
-    lib.setSourcePath("test-compiler");
-    lib.getSourceFileExtension = () => ".js";
     var classFile = new qx.tool.compiler.ClassFile(analyser, "classIssue519", lib);
     classFile.load(() => {
       var dbClassInfo = {};
@@ -19,4 +20,18 @@ test("test issue 519", assert => {
   }catch(ex) {
     assert.end(ex);
   }
-})
+});
+
+test("test issue 524", assert => {
+  try {  
+    var classFile = new qx.tool.compiler.ClassFile(analyser, "classIssue524", lib);
+    classFile.load(() => {
+      var dbClassInfo = {};
+      classFile.writeDbInfo(dbClassInfo);
+      assert.ok(!dbClassInfo.unresolved)
+      assert.end();
+    });
+  }catch(ex) {
+    assert.end(ex);
+  }
+});
