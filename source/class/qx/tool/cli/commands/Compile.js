f1028997f0c13c6adfc8dc0980c8144d548b7d73
Henner Kollmann <henner.kollmann@gmx.de>
2020-01-08 21:14:58 +0100

    remove unessary message - in a multi app configuration it's normal to have no default app

--- source/class/qx/tool/cli/commands/Compile.js
+++ source/class/qx/tool/cli/commands/Compile.js
@@ -616,7 +616,6 @@ qx.Class.define("qx.tool.cli.commands.Compile", {
           });
           if (!hasExplicitDefaultApp && (targetConfig.appConfigs.length > 1)) {
             targetConfig.defaultAppConfig = targetConfig.appConfigs[0];
-            qx.tool.compiler.Console.print("qx.tool.cli.compile.selectingDefaultApp", targetConfig.defaultAppConfig.name);
           }
         }
       });
