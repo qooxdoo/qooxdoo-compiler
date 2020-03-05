ecef4fe8948486af5e3e74390fec918f51ea7436
John Spackman <john.spackman@zenesis.com>
2020-03-05 10:02:11 +0000

    demonstration code for fixes https://github.com/qooxdoo/qooxdoo-compiler/issues/585

---
+++ test/issue585/source/class/issue585/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2020 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("issue585.theme.Appearance",
+{
+  extend : qx.theme.indigo.Appearance,
+
+  appearances :
+  {
+  }
+});
