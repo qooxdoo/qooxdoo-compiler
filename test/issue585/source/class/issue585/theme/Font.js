1a190e42ccf0cc85356b072e8642c859398c1b13
John Spackman <john.spackman@zenesis.com>
2020-03-05 10:02:11 +0000

    demonstration code for fixes https://github.com/qooxdoo/qooxdoo-compiler/issues/585
    (cherry picked from commit ecef4fe8948486af5e3e74390fec918f51ea7436)
    (cherry picked from commit 977085b310da41d18af0e165b14858f7a60686a8)

---
+++ test/issue585/source/class/issue585/theme/Font.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2020 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("issue585.theme.Font",
+{
+  extend : qx.theme.indigo.Font,
+
+  fonts :
+  {
+  }
+});
