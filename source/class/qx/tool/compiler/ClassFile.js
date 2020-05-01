e928de0be621251f94c8d7c21185319fcef12817
Henner Kollmann <henner.kollmann@gmx.de>
2020-05-01 21:03:53 +0200

    make source map files relative again

--- source/class/qx/tool/compiler/ClassFile.js
+++ source/class/qx/tool/compiler/ClassFile.js
@@ -29,7 +29,7 @@ var util = require("./util");
 var types = require("@babel/types");
 var babylon = require("@babel/parser");
 var async = require("async");
-var pathModule = require("path");
+var pathModule = require("upath");
 require("./jsdoc/Parser");
 
 var log = util.createLog("analyser");
@@ -339,7 +339,7 @@ qx.Class.define("qx.tool.compiler.ClassFile", {
           let myPlugins = t._babelClassPlugins();
           var config = {
             babelrc: false,
-            sourceFileName : t.getSourcePath(),
+            sourceFileName : pathModule.relative("",t.getSourcePath()),
             filename: t.getSourcePath(),
             sourceMaps: true,
             "presets": [
