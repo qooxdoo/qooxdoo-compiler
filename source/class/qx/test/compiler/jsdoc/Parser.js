25e64cc8f4bc6941c832c07c713b38265baeb30c
Henner Kollmann <henner.kollmann@gmx.de>
2020-06-17 17:06:46 +0200

    fix comments from @cboulanger

--- source/class/qx/test/compiler/jsdoc/Parser.js
+++ source/class/qx/test/compiler/jsdoc/Parser.js
@@ -13,56 +13,10 @@
 
 ************************************************************************ */
 
-qx.Class.define("qx.test.tool.utils.Utils", {
+qx.Class.define("qx.test.compiler.jsdoc.Parser", {
   extend : qx.dev.unit.TestCase,
 
   members :
   {
-    testStripSourceMapWriteStreamWholeStream : async function() {
-      let ss = new qx.tool.utils.Utils.ToStringWriteStream();
-      let ws = new qx.tool.utils.Utils.StripSourceMapTransform();
-      ws.pipe(ss);
-      await new Promise(resolve => {
-        ws.on("finish", () => {
-          resolve();
-        });
-        ws.write("abc\ndef\n//# sourceMappingURL=IApplication.js.map?dt=1587127076441\nghi");
-        ws.end();
-      });
-      this.assertTrue(ss.toString() == "abc\ndef\nghi");
-    },
-
-    testStripSourceMapWriteStreamChunked1 : async function() {
-      let ss = new qx.tool.utils.Utils.ToStringWriteStream();
-      let ws = new qx.tool.utils.Utils.StripSourceMapTransform();
-      ws.pipe(ss);
-
-      await new Promise(resolve => {
-        ws.on("finish", () => {
-          resolve();
-        });
-        ws.write("abc\ndef\n//# source");
-        ws.write("MappingURL=IApplication.js.map?dt=1587127076441\nghi\njkl");
-        ws.end();
-      });
-      this.assertTrue(ss.toString() == "abc\ndef\nghi\njkl");
-    },
-
-    testStripSourceMapWriteStreamChunked2 : async function() {
-      let ss = new qx.tool.utils.Utils.ToStringWriteStream();
-      let ws = new qx.tool.utils.Utils.StripSourceMapTransform();
-      ws.pipe(ss);
-      
-      await new Promise(resolve => {
-        ws.on("finish", () => {
-          resolve();
-        });
-        ws.write("abc\ndef\n//# source");
-        ws.write("MappingURL=IApplication.js.map?dt=1587127076441");
-        ws.write("\nghi");
-        ws.end();
-      });
-      this.assertTrue(ss.toString() == "abc\ndef\nghi");
-    }
   }
 });
