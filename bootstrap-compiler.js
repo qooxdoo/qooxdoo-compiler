c9499d7eebea7a4f5d05376310cf42a4009b9d4d
John Spackman <john.spackman@zenesis.com>
2020-12-19 10:18:36 +0000

    workflow diags

--- bootstrap-compiler.js
+++ bootstrap-compiler.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 const utils = require("./bin/tools/utils");
+const fs = require("fs");
 
 (async function() {
+  console.log("pwd=" + process.cwd());
+  let files = await fs.promises.readdir(".");
+  console.log("files=" + files.join("\n  "));
   await utils.bootstrapCompiler();
   
   process.exit(0);
